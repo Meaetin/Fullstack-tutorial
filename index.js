c7f0cbd3ff490423569510fc69127cd6ee3299ff
Martin <martinteoyz@gmail.com>
2023-08-31 16:23:22 +0800

    second commit for testing

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 import express from "express";
+import "./models/user.js"
+import  "./services/passport.js"
+import authRoutes from "./routes/authRoutes.js"
+import keys from "./config/keys.js";
+import mongoose from "mongoose";
+import cookieSession from "cookie-session";
+import passport from "passport";
+
+mongoose.connect(keys.mongoURI);
+
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Hi there!");
-})
+// Tells app to use cookie 
+app.use(
+    cookieSession({
+        maxAge: 30 * 24 * 60 * 60 * 1000, // Cookie to last for 30 days
+        keys: [keys.cookieKey]
+    })
+);
 
-app.listen(5000);
+app.use(passport.initialize());
+app.use(passport.session());
+
+authRoutes(app);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT);
